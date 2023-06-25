2f8d698609f0525820812be140e6336d7e026ecf
ravi rawat <ravirawat0@outlook.com>
2023-06-25 17:05:29 +0530

    m:Added new demo-modal-project

---
+++ demo-models-project/src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import AddUser from "./components/Users/AddUsers";
+import UserList from "./components/Users/UserList";
+
+function App() {
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+  return (
+    <div className="App">
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userList} />
+    </div>
+  );
+}
+
+export default App;
